48131ce1e12b5120472172f2fa439c12f11bf9a8
rayan-ahamadi <115161695+rayan-ahamadi@users.noreply.github.com>
2023-03-22 11:52:23 +0100

    Problème d'accent dans le fichier d'importation de la base de données, fichier modifié

--- produit.py
+++ produit.py
@@ -7,10 +7,10 @@ class Produit():
         self.categ = categ_
     
     def addProduct(self,cursor):
-        query = f"INSERT INTO produit (nom,description,prix,quantité,id_categorie) VALUES ('{self.name}','{self.descri}',{self.price},{self.quantity},{self.categ});"
+        query = f"INSERT INTO produit (nom,description,prix,quantite,id_categorie) VALUES ('{self.name}','{self.descri}',{self.price},{self.quantity},{self.categ});"
         cursor.execute(query)
 
     def changeProduct(self,cursor,id):
-        query =  query = f"UPDATE produit SET nom = '{self.name}', description = '{self.descri}',prix = {self.price}, quantité = {self.quantity},id_categorie = {self.categ} WHERE id = {id};"
+        query =  query = f"UPDATE produit SET nom = '{self.name}', description = '{self.descri}',prix = {self.price}, quantite = {self.quantity},id_categorie = {self.categ} WHERE id = {id};"
         cursor.execute(query)
         
